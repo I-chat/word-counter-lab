9cd91c48ef48c5358502787f02fa8bd83e5177f4
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-10 04:33:02 +0100

    added comments to app

--- app/wordCounter.js
+++ app/wordCounter.js
@@ -1,9 +1,9 @@
 var wordCounter = function (word) {
-  var wordArray = word.split(/\s+/);
+  var wordArray = word.split(/\s+/);  // splits string into an array
   var counter = {};
-  for (var i in wordArray) {
+  for (var i in wordArray) { 
     if (wordArray[i] !== '') {
-      if (counter.hasOwnProperty(wordArray[i])) {
+      if (counter.hasOwnProperty(wordArray[i])) { //checks if object has property wordArray[i]
         counter[wordArray[i]] += 1;
       } else{
         counter[wordArray[i]] = 1;
